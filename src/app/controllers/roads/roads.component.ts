06ca96fb3b26d5c1f166d8874b4d9292dad37e71
Jakub Dziwura <jakub.dziwura@gmail.com>
2019-02-03 15:04:52 +0100

    Dodanie filtrowania wyników na mapie

--- src/app/controllers/roads/roads.component.ts
+++ src/app/controllers/roads/roads.component.ts
@@ -7,10 +7,12 @@ import * as Terraformer from 'terraformer-wkt-parser';
 import * as $ from 'jquery';
 
 $(() => {
-  var content = $('#context');  
+  var content = $('#context');
+  var filter = $('#filter');
   content.height(window.innerHeight - content.position().top);
   $(window).resize(() => {
     content.height(window.innerHeight - content.position().top);
+    content.css('min-height', filter.height() + 'px');
   })
 })
 
@@ -27,12 +29,12 @@ export class RoadsComponent implements OnInit {
 
   streetSections: Road[];
   filteredStreetSections: Road[];
-  boundary: any;  
+  boundary: any;
   searchText: string;
   oneway: boolean = false;
   surface: string;
   lanes: string;
-  maxspeed: string;  
+  maxspeed: string;
   waiting: boolean = false;
 
   map: Leaflet.Map = null;
@@ -82,7 +84,7 @@ export class RoadsComponent implements OnInit {
     var geoJson = Leaflet.geoJSON(Terraformer.parse(data), this.boundStyle);
     geoJson.addTo(this.map);
     this.map.fitBounds(geoJson.getBounds());
-  } 
+  }
 
   showSelectedSection(selectedSection: Road) {
     if (this.selectedSection)
@@ -93,16 +95,9 @@ export class RoadsComponent implements OnInit {
   }
 
   searchByText() {
-    if (this.section) {
-      this.section.removeFrom(this.map);
-      this.section = null;
-    }
-    if (this.selectedSection) {
-      this.selectedSection.removeFrom(this.map);
-      this.selectedSection = null;
-    }
+    this.clearMapFromSelection();
     if (this.searchText.match(new RegExp(searchFromToPattern))) {
-      var streets = this.searchText.match(new RegExp(streetPattern,'g'));
+      var streets = this.searchText.match(new RegExp(streetPattern, 'g'));
       this.waiting = true;
       this.roadService.getRoadFromTo({ street: streets[0], street_from: streets[1], street_to: streets[2] }).subscribe((result) => {
         this.handleSerachResult(result);
@@ -119,38 +114,84 @@ export class RoadsComponent implements OnInit {
     }
   }
 
-  handleSerachResult(result) {    
+  handleSerachResult(result) {
+    this.clearFilters(false);
     this.streetSections = JSON.parse(result);
+    this.filteredStreetSections = this.streetSections;
     if (this.streetSections.length != 0 && this.map) {
-      this.section = Leaflet.featureGroup(this.streetSections.map(road => {
-        return Leaflet.geoJSON(JSON.parse(road.object), this.sectionStyle)
-      }));
-      this.section.addTo(this.map);
-      this.map.fitBounds(this.section.getBounds());
+      this.showResultOnMap();
+    }
+  }
+
+  showResultOnMap() {
+    this.section = Leaflet.featureGroup(this.filteredStreetSections.map(road => {
+      return Leaflet.geoJSON(JSON.parse(road.object), this.sectionStyle)
+    }));
+    this.section.addTo(this.map);
+    this.map.fitBounds(this.section.getBounds());
+  }
+  
+  clearMapFromSelection() {
+    if (this.section) {
+      this.section.removeFrom(this.map);
+      this.section = null;
+    }
+    if (this.selectedSection) {
+      this.selectedSection.removeFrom(this.map);
+      this.selectedSection = null;
     }
   }
 
   surfaceMap(surface) {
-    if(surface === 'asphalt')
+    if (surface === 'asphalt')
       return 'Asfalt';
-    if(surface === 'concrete')
+    if (surface === 'concrete')
       return 'Beton';
-    if(surface === 'paving_stones')
+    if (surface === 'paving_stones')
       return 'Kostka Brukowa';
-    if(surface === 'unpaved')
+    if (surface === 'unpaved')
       return 'Nieutwardzona';
-    if(surface === 'cobblestone' || surface === 'sett')
+    if (surface === 'cobblestone' || surface === 'sett')
       return 'Bruk'
-    if(surface)
+    if (surface)
       return `Inna(${surface})`;
     return 'Brak informacji';
   }
 
-  clearFilters() {
+  filterData() {
+    if (this.streetSections) {
+      this.filteredStreetSections = this.streetSections.filter(road => {
+        var result = true;
+        if (this.surface)
+          result = result && (this.surface == road.surface);
+        if (this.maxspeed)
+          result = result && (this.maxspeed == road.maxspeed);
+        if (this.lanes) {
+          if (this.lanes == '3')
+            result = result && (road.lanes >= this.lanes);
+          else
+            result = result && (road.lanes == this.lanes);
+        }
+        if (this.oneway)
+          result = result && (road.oneway == 'yes');
+        return result;
+      })
+
+      this.clearMapFromSelection();
+      this.showResultOnMap();
+    }
+  }
+
+  clearFilters(rewrite: boolean) {
     this.surface = '';
     this.maxspeed = '';
     this.lanes = '';
     this.oneway = false;
+    this.filteredStreetSections = this.streetSections;
+    if (rewrite && this.streetSections) {
+      this.clearMapFromSelection();
+      this.showResultOnMap();
+    }
   }
 }
 
